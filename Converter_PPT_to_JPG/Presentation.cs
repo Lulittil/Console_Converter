e84cb21ee57966d97cd509ce64845a31a0462dde
Lulittil <llullittill@gmail.com>
2021-09-07 23:54:34 +0500

    Console converter pptx to jpg

---
+++ Converter_PPT_to_JPG/Presentation.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+
+namespace Converter_PPT_to_JPG
+{
+    class Presentation
+    {
+        private static string presentation_path { get; set; }
+        private static string finish_path { get; set; }
+        private Microsoft.Office.Interop.PowerPoint.Presentation pres;
+        private Microsoft.Office.Interop.PowerPoint.Application application_ppt;
+
+        public Presentation(string pres, string finish)
+        {
+            presentation_path = pres;
+            finish_path = finish;
+            application_ppt = new Microsoft.Office.Interop.PowerPoint.Application(); 
+        }
+
+        
+            //foreach (Microsoft.Office.Interop.PowerPoint.Slide objSlide in pres.Slides)
+            //    {
+            //        objSlide.Export(finish_path + @"\Slide_" + i + $"_{name_present}" + ".JPG", "JPG", 960, 720);
+            //            i++;
+            //}
+        
+        void Download()
+        {
+            try
+            {
+
+
+                Microsoft.Office.Interop.PowerPoint.Application application_ppt = new Microsoft.Office.Interop.PowerPoint.Application();
+                Microsoft.Office.Interop.PowerPoint.Presentation pres = application_ppt.Presentations.Open(presentation_path, Microsoft.Office.Core.MsoTriState.msoCTrue, Microsoft.Office.Core.MsoTriState.msoTriStateMixed, Microsoft.Office.Core.MsoTriState.msoFalse);
+                string name_present = pres.Name;
+
+                name_present = name_present.Substring(0, name_present.Length - 5);
+                int i = 0;
+                //var tasks = new List<Task>();
+                foreach (Microsoft.Office.Interop.PowerPoint.Slide objSlide in pres.Slides)
+                {
+                    // tasks.Add(ExportT(objSlide,i));
+                    objSlide.Export(finish_path + @"\Slide_" + i + $"_{name_present}" + ".JPG", "JPG", 960, 720);
+                    Thread.Sleep(1000);
+                    i++;
+                }
+                pres.Close();
+                application_ppt.Quit();
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+
+        public Task Download_t()
+        {
+            return Task.Run(() => {
+                Download();
+            });
+        }
+        
+
+        public async Task ExpTask()
+        {
+
+            await Task.Run(() => { Download(); });
+            //try
+            //{
+                
+
+            //        Microsoft.Office.Interop.PowerPoint.Application application_ppt = new Microsoft.Office.Interop.PowerPoint.Application();
+            //        Microsoft.Office.Interop.PowerPoint.Presentation pres = application_ppt.Presentations.Open(presentation_path, Microsoft.Office.Core.MsoTriState.msoCTrue, Microsoft.Office.Core.MsoTriState.msoTriStateMixed, Microsoft.Office.Core.MsoTriState.msoFalse);
+            //        string name_present = pres.Name;
+
+            //        name_present = name_present.Substring(0, name_present.Length - 5);
+            //        int i = 0;
+            //        var tasks = new List<Task>();
+            //        foreach (Microsoft.Office.Interop.PowerPoint.Slide objSlide in pres.Slides)
+            //        {
+            //           // tasks.Add(ExportT(objSlide,i));
+            //            objSlide.Export(finish_path + @"\Slide_" + i + $"_{name_present}" + ".JPG", "JPG", 960, 720);
+            //            //var inner = Task.Factory.StartNew(() => { Export(objSlide, i); });
+            //            i++;
+            //        }
+            //        //await Task.WhenAll(tasks);
+            //        pres.Close();
+            //        application_ppt.Quit();
+               
+                
+                
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("Error: " + ex.Message);
+            //}
+        }
+
+        public Task ExportT(Microsoft.Office.Interop.PowerPoint.Slide objSlide,int i)
+        {
+            return Task.Run(()=> {
+                objSlide.Export(finish_path + @"\Slide_" + i + $"_" + ".JPG", "JPG", 960, 720);
+            }); 
+        }
+
+        
+
+
+        public void convert_ppt_to_jpg()
+        {
+            try
+            {
+                Microsoft.Office.Interop.PowerPoint.Application application_ppt = new Microsoft.Office.Interop.PowerPoint.Application();
+                Microsoft.Office.Interop.PowerPoint.Presentation pres = application_ppt.Presentations.Open(presentation_path,Microsoft.Office.Core.MsoTriState.msoCTrue, Microsoft.Office.Core.MsoTriState.msoTriStateMixed, Microsoft.Office.Core.MsoTriState.msoFalse);
+               //;
+                //name_present = name_present.Substring(0, name_present.Length - 5);
+                int i = 0;
+                //Console.WriteLine(name_present);
+                foreach (Microsoft.Office.Interop.PowerPoint.Slide objSlide in pres.Slides)
+                {
+                    //objSlide.Export(finish_path + @"\Slide_" + i + $"_{name_present}" + ".JPG", "JPG", 960, 720);
+                    //Export(objSlide, i);
+                    i++;
+                }
+                pres.Close();
+                application_ppt.Quit();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+        }
+
+        
+    }
+}
---
+++ Converter_PPT_to_JPG/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using System.Reflection;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+
+namespace Converter_PPT_to_JPG
+{
+    class Program
+    {
+
+        
+
+        public static Task Download_Task(string firstpath,string finishpath)
+        {
+            return Task.Run(()=> 
+            {
+                Microsoft.Office.Interop.PowerPoint.Application application_ppt = new Microsoft.Office.Interop.PowerPoint.Application();
+                Microsoft.Office.Interop.PowerPoint.Presentation pres = application_ppt.Presentations.Open(firstpath, Microsoft.Office.Core.MsoTriState.msoCTrue, Microsoft.Office.Core.MsoTriState.msoTriStateMixed, Microsoft.Office.Core.MsoTriState.msoFalse);
+                //string name_present = pres.Name;
+
+                //name_present = name_present.Substring(0, name_present.Length - 5);
+                int i = 0;
+                foreach (Microsoft.Office.Interop.PowerPoint.Slide objSlide in pres.Slides)
+                {
+                    objSlide.Export(finishpath + @"\Slide_" + i  + ".JPG", "JPG", 960, 720);
+                    i++;
+                }
+                pres.Close();
+                application_ppt.Quit();
+            });
+        }
+
+
+        static void Main(string[] args)
+        {
+
+           
+            for(int i=1;i<=2;i++)
+            {
+                
+                Console.WriteLine("Введите путь до презентации: ");
+                string fp=Console.ReadLine();
+                Console.WriteLine("Введите путь конвертации: ");
+                string sp =Console.ReadLine();
+
+                var t3 = Download_Task(fp, sp);
+            }
+            
+
+            Console.ReadKey(true);
+
+        }
+    }
+}
